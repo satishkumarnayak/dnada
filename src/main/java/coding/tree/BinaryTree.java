a29068965aaef0587335357804d885aa21c09ab3
Satish Nayak <satish_nayak@epam.com>
2021-04-06 16:15:22 +0530

    Binary Sorted Tree - Delete Node

--- src/main/java/coding/tree/BinaryTree.java
+++ src/main/java/coding/tree/BinaryTree.java
@@ -73,16 +73,91 @@ public class BinaryTree {
     return current.key;
 
   }
-  
+
   public int max() {
     if (root == null) {
       return -1;
     }
     Node current = root;
-    while(current.right != null) {
+    while (current.right != null) {
       current = current.right;
     }
     return current.key;
+  }
+
+  public void delete(int key) {
+
+    Node nodeToDel = null;
+    nodeToDel = findNode(root, key);
+
+  }
+
+
+  private Node findNode(Node node, int key) {
+    if (node == null) {
+      return null;
+    } else if (key < node.key) {
+      node.left = findNode(node.left, key);
+    } else if (key > node.key) {
+      node.right = findNode(node.right, key);
+    } else {
+      // case 1 : with no child
+      if (node.left == null && node.right == null) {
+        node = null;
+      }
+
+      // case 2 : with one child
+      else if (node.left == null && node.right != null) {
+        node = node.right;
+        node.right = null;
+      } else if (node.right == null && node.left != null) {
+        node = node.left;
+        node.left = null;
+      }
+      // case 3 : with two child
+      else if (node.left != null && node.right != null) {
+          // find minimum node
+        Node current = node;  
+        while(current != null && current.left != null) {
+          current = current.left;
+        }        
+        // swap find node and min code
+        node = current;
+        current = null;
+      }
+
+    }
+    return node;
+  }
+
+  public void prettyPrint() {
+    // Hard coded print out of binary tree depth = 3
+
+    int rootLeftKey = root.left == null ? 0 : root.left.key;
+    int rootRightKey = root.right == null ? 0 : root.right.key;
+
+    int rootLeftLeftKey = 0;
+    int rootLeftRightKey = 0;
+
+    if (root.left != null) {
+      rootLeftLeftKey = root.left.left == null ? 0 : root.left.left.key;
+      rootLeftRightKey = root.left.right == null ? 0 : root.left.right.key;
+    }
+
+    int rootRightLeftKey = 0;
+    int rootRightRightKey = 0;
+
+    if (root.right != null) {
+      rootRightLeftKey = root.right.left == null ? 0 : root.right.left.key;
+      rootRightRightKey = root.right.right == null ? 0 : root.right.right.key;
+    }
+
+    System.out.println("     " + root.key);
+    System.out.println("   /  \\");
+    System.out.println("  " + rootLeftKey + "    " + rootRightKey);
+    System.out.println(" / \\  / \\");
+    System.out.println(rootLeftLeftKey + "  " + rootLeftRightKey + " " + rootRightLeftKey + "   "
+        + rootRightRightKey);
   }
 
 }
--- src/test/java/coding/BinaryTreeTest.java
+++ src/test/java/coding/BinaryTreeTest.java
@@ -14,8 +14,8 @@ public class BinaryTreeTest {
     tree = new BinaryTree();
   }
 
-  @Test
-  public void insertTest() {
+ // @Test
+  public void treeTest() {
     tree.insert(5, "data1");
     tree.insert(3, "data1113");
     tree.insert(7, "data131s");
@@ -25,12 +25,95 @@ public class BinaryTreeTest {
     tree.insert(8, "data198");
 
     assertEquals(true, tree.binarySearch(5));
-    assertEquals(false, tree.binarySearch(10));
     assertEquals(true, tree.binarySearch(6));
     
     assertEquals(2, tree.min());
     assertEquals(8, tree.max());
+    
+    assertEquals(true, tree.binarySearch(2));
+    tree.prettyPrint();
+
+
+    
+   
   
+  }
+  
+ // @Test
+  public void treeDelNoChildTest() {
+    tree.insert(5, "data1");
+    tree.insert(3, "data1113");
+    tree.insert(7, "data131s");
+    tree.insert(2, "data113");
+    tree.insert(4, "data143");
+    tree.insert(6, "data167");
+    tree.insert(8, "data198");
+
+    tree.prettyPrint();
+
+    tree.delete(2);
+    assertEquals(false, tree.binarySearch(2));
+    tree.delete(8);
+    assertEquals(false, tree.binarySearch(8));
+    tree.prettyPrint();
+
+  }
+  
+ // @Test
+  public void treeDelOneChildTest() {
+    tree.insert(5, "data1");
+    tree.insert(3, "data1113");
+    tree.insert(7, "data131s");
+    tree.insert(2, "data113");
+    tree.insert(4, "data143");
+    tree.insert(6, "data167");
+    tree.insert(8, "data198");
+
+    tree.prettyPrint();
+
+    tree.delete(2);
+    assertEquals(false, tree.binarySearch(2));
+    tree.delete(3);
+    assertEquals(false, tree.binarySearch(3));
+    tree.prettyPrint();
+
+  }
+  
+ // @Test
+  public void treeDelOneChild2Test() {
+    tree.insert(5, "data1");
+    tree.insert(3, "data1113");
+    tree.insert(7, "data131s");
+    tree.insert(2, "data113");
+    tree.insert(4, "data143");
+    tree.insert(6, "data167");
+    tree.insert(8, "data198");
+
+    tree.prettyPrint();
+
+    tree.delete(8);
+    assertEquals(false, tree.binarySearch(8));
+    tree.delete(7);
+    assertEquals(false, tree.binarySearch(7));
+    tree.prettyPrint();
+
+  }
+  
+  @Test
+  public void treeDelTwoChildTest() {
+    tree.insert(5, "data1");
+    tree.insert(3, "data1113");
+    tree.insert(7, "data131s");
+    tree.insert(2, "data113");
+    tree.insert(4, "data143");
+    tree.insert(6, "data167");
+    tree.insert(8, "data198");
+
+    tree.prettyPrint();
+    tree.delete(7);
+    assertEquals(false, tree.binarySearch(7));
+    tree.prettyPrint();
+
   }
 
 }
